a07c1a5e8a62d5cc8c80e97532af36855478b450
2005869 <2005869@aluno.univesp.br>
2022-12-06 17:14:29 -0300

    add class Reader and Read function

---
+++ Reader.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+class Reader{
+
+    Read(filepath){
+        fs.readFile(filepath, {encoding: 'utf-8'}, (err, data) => {
+            if(err){
+                console.log(err);
+            }else{
+                console.log(data);
+            }
+        });
+    }
+}
