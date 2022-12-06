aeccc91db04a848286af8882f85f7f4b81fc1f38
2005869 <2005869@aluno.univesp.br>
2022-12-06 17:55:42 -0300

    convert Read to async await

--- Reader.js
+++ Reader.js
@@ -1,15 +1,18 @@
 const fs = require('fs');
+const util = require('util');
 
 class Reader{
+    constructor(){
+        this.reader = util.promisify(fs.readFile);
+    }
 
-    Read(filepath){
-        fs.readFile(filepath, {encoding: 'utf-8'}, (err, data) => {
-            if(err){
-                console.log(err);
-            }else{
-                console.log(data);
-            }
-        });
+    async Read(filepath){
+        try{
+            return await this.reader(filepath, 'utf8');
+        }catch(err){
+            return undefined;
+        }
+        
     }
 }
 
