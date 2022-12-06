f9e5d17bc608bed2351a0d582d252514bcd40385
2005869 <2005869@aluno.univesp.br>
2022-12-06 18:13:36 -0300

    add main app

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var Reader = require('./Reader');
+var Processor = require('./Processor');
 
 var leitor = new Reader();
 
 async function main(){
     var dados = await leitor.Read('./users.csv');
+    var dadosProcessados = Processor.Process(dados);
 }
 
 main();
