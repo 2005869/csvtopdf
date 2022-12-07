deb1364b19ef497c89c1a1674006b5d2a8410534
2005869 <2005869@aluno.univesp.br>
2022-12-07 20:28:50 -0300

    options for PDFWriter

--- PDFWriter.js
+++ PDFWriter.js
@@ -1,8 +1,22 @@
 var pdf = require('html-pdf');
 
+const options = {
+    childProcessOptions: {
+      env: {
+        OPENSSL_CONF: '/dev/null',
+      },
+    }
+  }
+
 class PDFWriter{
     static WritePDF(filename, html){
-        pdf.create(html, {}).toFile(filename)
+        pdf.create(html, options).toFile(filename, (err, res) => {
+            if (err){
+                console.log(err);
+            }else{
+                console.log(res);
+            }
+        });
     }
 }
 
